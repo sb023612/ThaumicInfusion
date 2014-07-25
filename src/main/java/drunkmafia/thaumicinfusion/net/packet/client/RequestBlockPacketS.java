e9581ab0172301257b68da4df09a381fe57a03fb
DrunkMafia <sean.hall412@gmail.com>
2014-07-25 20:30:56 +0100

    Implemented a packet timeout, a block can now only request after the last pack it sent has timed out

--- src/main/java/drunkmafia/thaumicinfusion/net/packet/client/RequestBlockPacketS.java
+++ src/main/java/drunkmafia/thaumicinfusion/net/packet/client/RequestBlockPacketS.java
@@ -1,14 +1,22 @@
 package drunkmafia.thaumicinfusion.net.packet.client;
 
+import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import drunkmafia.thaumicinfusion.common.block.InfusedBlock;
 import drunkmafia.thaumicinfusion.common.util.BlockHelper;
 import drunkmafia.thaumicinfusion.common.util.BlockSavable;
 import drunkmafia.thaumicinfusion.net.ChannelHandler;
 import drunkmafia.thaumicinfusion.net.packet.server.BlockSyncPacketC;
 import io.netty.buffer.ByteBuf;
+import net.minecraft.block.Block;
 import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.world.World;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by DrunkMafia on 28/06/2014.
@@ -22,20 +30,38 @@ public class RequestBlockPacketS implements IMessage {
 
     private ChunkCoordinates coordinates;
 
+    public static HashMap<ChunkCoordinates, Long> syncTimeouts = new HashMap<ChunkCoordinates, Long>();
+    private static long maxTimeout = 60000;
+
     public RequestBlockPacketS(ChunkCoordinates coordinates) {
-        this.coordinates = coordinates;
+        Block block = FMLClientHandler.instance().getClient().theWorld.getBlock(coordinates.posX, coordinates.posY, coordinates.posZ);
+        if(block != null && block instanceof InfusedBlock) {
+            if (syncTimeouts.containsKey(coordinates)) {
+                long timeout = syncTimeouts.get(coordinates);
+                if ((timeout + maxTimeout) < System.currentTimeMillis()) {
+                    syncTimeouts.put(coordinates, System.currentTimeMillis());
+                    this.coordinates = coordinates;
+                }
+            } else {
+                syncTimeouts.put(coordinates, System.currentTimeMillis());
+                this.coordinates = coordinates;
+            }
+        }
     }
 
     @Override
     public void fromBytes(ByteBuf buf) {
-        coordinates = new ChunkCoordinates(buf.readInt(), buf.readInt(), buf.readInt());
+        if(buf.readByte() == 1) coordinates = new ChunkCoordinates(buf.readInt(), buf.readInt(), buf.readInt());
     }
 
     @Override
     public void toBytes(ByteBuf buf) {
-        buf.writeInt(coordinates.posX);
-        buf.writeInt(coordinates.posY);
-        buf.writeInt(coordinates.posZ);
+        if(coordinates != null) {
+            buf.writeByte(1);
+            buf.writeInt(coordinates.posX);
+            buf.writeInt(coordinates.posY);
+            buf.writeInt(coordinates.posZ);
+        }else buf.writeByte(0);
     }
 
     public static class Handler implements IMessageHandler<RequestBlockPacketS, IMessage> {
